c76a52510f5c97840c7aea15a8619178e9b646d4
fahizrahmank <fahizrahmank@gmail.com>
2024-05-01 12:37:05 +0530

    update

--- src/Components/Accordian/index.jsx
+++ src/Components/Accordian/index.jsx
@@ -6,17 +6,31 @@ import data from "../data";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
   const handleSingleSelection = (getCurrentId) => {
     console.log(getCurrentId);
     setSelected(getCurrentId === selected ? null : getCurrentId);
   };
+  const handleMultiSeection = (getCurrentId) => {
+
+  }
 
   return (
     <div className="wrapper">
+      <button
+        style={{ fontWeight: "bolder" }}
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+      >
+        Enable Multi selecton
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((item) => (
-            <div className="item">
+            <div
+              className="item"
+              style={{ backgroundColor: "yellowgreen", fontWeight: "bolder" }}
+            >
               <div
                 onClick={() => handleSingleSelection(item.id)}
                 className="title"
@@ -24,16 +38,17 @@ const Accordian = () => {
                 <h3> {item.name}</h3>
                 <span>+</span>
               </div>
-              {selected == item.id ? 
-              
-              <div className="content"> {item.email} <br /> {item.phone}</div>
-              :null}
+              {selected == item.id ? (
+                <div className="content">
+                  {" "}
+                  {item.email} <br /> {item.phone}
+                </div>
+              ) : null}
             </div>
-            
           ))
         ) : (
           <div>
-            <h1>no data found in this folder !!!!</h1>
+            <h1>No data found in this folder !!!!</h1>
             <p>please try agian later</p>
             <button>ok</button>
             <button>Retry</button>
