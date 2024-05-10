70cbfddee30667f0e93c9c6b9c2f4479382adb9b
fahizrahmank <fahizrahmank@gmail.com>
2024-05-10 11:58:07 +0530

    update

--- src/Components/RandomColor/index.jsx
+++ src/Components/RandomColor/index.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const index = () => {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
-  function randomColorUtility(length){
-    return Math.floor(Math.random()*length)
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
   }
 
-
   const handleCreateRandomHexColor = () => {
-    const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    let hexColor = "#"
-    for(let i = 0 ; i<6; i++){
-      hexColor += hex[randomColorUtility(hex.length)]
-
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
     }
     console.log(hexColor);
-    setColor(hexColor)
+    setColor(hexColor);
   };
 
-  const handleCreateRandomRGBColor = () => {};
+  const handleCreateRandomRGBColor = () => {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+    setColor(`rgb (${r},${g},${b})`);  
+  };
+  useEffect(() => {
+    if (typeOfColor === "rgb") handleCreateRandomRGBColor();
+    else handleCreateRandomHexColor();
+  }, [typeOfColor]);
 
   return (
     <div
@@ -40,6 +47,22 @@ const index = () => {
       >
         Generate Random Color
       </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#fff",
+          fontSize: "30px",
+          marginTop: "70",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <h3>{typeOfColor === "rgb" ? "RGB color" : "HEX color"}</h3>
+
+        <h1>{color}</h1>
+      </div>
     </div>
   );
 };
