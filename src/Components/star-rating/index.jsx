1072ac21d7891df955156ebe6e5f28e673054000
fahizrahmank <fahizrahmank@gmail.com>
2024-05-11 12:25:56 +0530

    update

--- src/Components/star-rating/index.jsx
+++ src/Components/star-rating/index.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import {FaStar} from 'react-icons/fa'
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
-const starRating = ({noOfStar = 5}) => {
+const starRating = ({ noOfStar = 5 }) => {
+  const [rating, setRaing] = useState(0);
+  const [hover, setHover] = useState(0);
+  function handleClick(getCurrentIndex) {
+    console.log(getCurrentIndex);
+  }
+  function handleMouseEnter(getCurrentIndex) {
+    console.log(getCurrentIndex);
+
+  }
+  function handleMouseLeave(getCurrentIndex) {
+    console.log(getCurrentIndex);
+
+  }
   return (
-    <div className='star-aing'>
-        
-        
-        {
-            [...Array(noOfStar)].map((_,index) => {
-                return <FaStar
-                key={index}
-            
-                
-                />
-            })
-        }
-        
-        </div>
-  )
-}
+    <div className="star-aing">
+      {[...Array(noOfStar)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default starRating
+export default starRating;
