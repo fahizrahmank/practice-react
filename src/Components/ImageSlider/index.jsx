480ac7589df0838fbc906301571ba01f1ca9a546
fahizrahmank <fahizrahmank@gmail.com>
2024-05-16 16:51:51 +0530

    update

--- src/Components/ImageSlider/index.jsx
+++ src/Components/ImageSlider/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const ImageSlider = () => {
-  return (
-    <div>ImageSlider</div>
-  )
-}
+const ImageSlider = ({ url, limit }) => {
+  const [image, setImage] = useState([]);
+  const [slide, setSlide] = useState(0);
+  const [errorMsg,setErrorMsg] = useState(null)
+  async function fetchImages(getUrl){
+    try {
+      
+    } catch (error) {
+      
+    }
 
-export default ImageSlider
+  }
+  useEffect(() => {
+    if (url !== " ") fetchImages(getUrl);
+  }, [url]);
+
+  return <div className="container">ImageSlider</div>;
+};
+
+export default ImageSlider;
